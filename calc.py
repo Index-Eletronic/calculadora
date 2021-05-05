95283caa6b0381e6ac20424c7372d7485f6b0d0d
Fil Tuon <index.eletronic@gmail.com>
2021-05-05 16:02:56 -0300

    .

--- calc.py
+++ calc.py
@@ -3,4 +3,29 @@ import funcoes
 funcoes.titulo('CALCULADORA DE PLANEJADOS', 2)
 
 
-funcoes.caixa()
+funcoes.opcao()
+
+perg = (int(input(f'Digite sua opção aqui : ').strip()))
+
+if perg == 0:
+    pass
+
+elif perg == 1:
+    funcoes.titulo('  opção para calcular a Caixa (Laterais/Base/Chápeu)'.upper())
+    funcoes.caixa()
+
+elif perg == 2:
+    pass
+elif perg == 3:
+    pass
+elif perg == 4:
+    pass
+elif perg == 5:
+    pass
+elif perg == 6:
+    pass
+elif perg == 7:
+    pass
+else:
+    funcoes.titulo('Valte sempre!')
+
--- funcoes.py
+++ funcoes.py
@@ -8,29 +8,41 @@ c = ('\033[m',         # cor 0 = sem cores
      '\033[7;30m',  # cor 6 = Branco
     )
 
+def opcao():
+   print('\033[0;30;41mDIGITE A OPÇÃO QUE DESEJA CALCULAR:\33[m')
+   print('''
+            [0] - calcular a plaejado todo
+            [1] - calcular area da caixa (laterais, base, chápeu e divisória central (se houver)).
+            [2] - calcular o fundo
+            [3] - Calular a(s) porta(s)
+            [4] - Calcular as gavetas
+            [5] - calcular as prateleiras
+            [6] - calcular os fiancos.
+            [7] - calcular um painel.
+        '''.upper())
 
-plan = list()
-plancopia = list()
-cont = 0
 def caixa():
-    while True:
+        plan = list()
+        plancopia = list()
         cont = 0
-        plan.append(str(input('O TIPO DE MATERIAL : '.replace('.', ','))))
-        plan.append(float(input('DIGITE:A ALTURA EM METROS : '.replace('.', ','))))
-        plan.append(float(input('LARGURA EM METROS : '.replace('.', ','))))
-        plan.append(float(input('QUANTIDADE : '.replace('.', ','))))
-        plancopia.append(plan[:])
-        mat = plancopia[0][0]
-        plan.clear()
-        area = (((plancopia[0][1] * plancopia[0][2]) * plancopia[0][3]))
-        #calc = (((plancopia[0][1] * plancopia[0][2]) * plancopia[0][3])/5.06)
-        cont += (area/5.06)
-        resp = str(input('TEM MAIS DA MESMA MEDIDA PARA ADICIONAR? [S/N]: '))
-        if resp not in 'Nn':
+        while True:
+            cont = 0
+            plan.append(str(input('O TIPO DE MATERIAL : '.replace('.', ','))))
+            plan.append(float(input('DIGITE:A ALTURA EM METROS : '.replace('.', ','))))
+            plan.append(float(input('LARGURA EM METROS : '.replace('.', ','))))
+            plan.append(float(input('QUANTIDADE : '.replace('.', ','))))
+            plancopia.append(plan[:])
+            mat = plancopia[0][0]
+            plan.clear()
+            area = (((plancopia[0][1] * plancopia[0][2]) * plancopia[0][3]))
+            #calc = (((plancopia[0][1] * plancopia[0][2]) * plancopia[0][3])/5.06)
+            cont += (area/5.06)
+            resp = str(input('TEM MAIS DA MESMA MEDIDA PARA ADICIONAR? [S/N]: '))
+            if resp not in 'Nn':
+                break
+            subtitulos(f'O metro quadrado total da Caixa é: {area:.2f} metros '.upper())
+            subtitulos(f'E serão necessários {area/5.06:.2f} CHapas de 2.75 x 1.84 do material {plancopia[0][0]} '.upper())
             break
-        print(f'O metro quadrado total da Caixa é: {area:.2f}')
-        print(f'E serão necessários {area/5.06:.2f} placas do material {plancopia[0][0]} para fabrica-lá')
-        break.
 
 
 #==============Calculando a caixa======================
@@ -41,3 +53,10 @@ def titulo(msg, cor=0):
         print(c[2], end='')
         print(f' {msg}\33[m')
         print('~' * tam)
+
+def subtitulos(msg):
+    print(c[5], end='')
+    print(f'{msg}\33[m')
+    print(c[5], end='')
+
+
